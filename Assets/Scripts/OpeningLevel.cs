1ad3e07d3fc953408d8fbf7cbefd04d48ba88b62
gkizior <gkizior@purdue.edu>
2016-02-29 18:58:58 -0500

    Random Level Generator

--- Assets/Scripts/OpeningLevel.cs
+++ Assets/Scripts/OpeningLevel.cs
@@ -38,19 +38,79 @@ public class OpeningLevel : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 
 	void loadLevel() {
 		tileColors = new Color[(levelWidth * levelHeight)];
 		tileColors = levelTexture.GetPixels ();
 		System.Random rand = new System.Random ();
-		int randx, randy;
-		for (int a = 0; a < 1000; a++) { 
-			randx = rand.Next (levelWidth / 4, (3 * levelWidth) / 4);
-			randy = rand.Next (levelWidth / 4, (3 * levelWidth) / 4);
+
+		print ("levelHeight = " + levelHeight);
+		print ("levelWidth = " + levelWidth);
+
+		//boundary left
+		for(int leftBW = 0; leftBW < 7; leftBW++) {
+			for(int leftBH = 0; leftBH < 64; leftBH++) {
+				Instantiate(stoneTile, new Vector3 ( leftBW, leftBH), Quaternion.identity);
+			}
+		}
+
+		//boundary right
+		for(int rightBW = 0; rightBW < 7; rightBW++) {
+			for(int rightBH = 0; rightBH < 64; rightBH++) {
+				Instantiate(stoneTile, new Vector3 ( 64 - rightBW, rightBH), Quaternion.identity);
+			}
+		}
+
+		//boundary down
+		for(int downBW = 3; downBW < 62; downBW++) {
+			for(int downBH = 0; downBH < 7; downBH++) {
+				Instantiate(stoneTile, new Vector3 ( downBW, downBH), Quaternion.identity);
+			}
+		}
+
+		//boundary up
+		for(int upBW = 3; upBW < 62; upBW++) {
+			for(int upBH = 57; upBH < 64; upBH++) {
+				Instantiate(stoneTile, new Vector3 ( upBW, upBH), Quaternion.identity);
+			}
+		}
+			
+		//random boundaries
+		int randx, randy, adjacent, howMany;
+		int divisor = 6;
+		int density = 100;
+		int maxWallLength = 4;
+		for (int a = 0; a < density; a++) { 
+			randx = rand.Next (levelWidth / divisor, ((divisor - 1) * levelWidth) / divisor);
+			randy = rand.Next (levelWidth / divisor, ((divisor - 1) * levelWidth) / divisor);
+			adjacent = rand.Next (0, 3);
+			howMany = rand.Next (1, maxWallLength);
+			if (adjacent == 0) {
+				//left
+				for (int i = 1; i <= howMany; i++) {
+					Instantiate (stoneTile, new Vector3 (randx - i, randy), Quaternion.identity);
+				}
+			} else if (adjacent == 1) {
+				//right
+				for (int i = 1; i <= howMany; i++) {
+					Instantiate (stoneTile, new Vector3 (randx + i, randy), Quaternion.identity);
+				}
+			} else if (adjacent == 2) {
+				//down
+				for (int i = 1; i <= howMany; i++) {
+					Instantiate (stoneTile, new Vector3 (randx, randy - i), Quaternion.identity);
+				}
+			} else if (adjacent == 3) {
+				//up
+				for (int i = 1; i <= howMany; i++) {
+					Instantiate (stoneTile, new Vector3 (randx, randy + i), Quaternion.identity);
+				}
+			}
 			Instantiate (stoneTile, new Vector3 (randx, randy), Quaternion.identity);
 		}
+
 		for (int y = 0; y < levelHeight; y++) {
 			for (int x = 0; x < levelWidth; x++) {
 
