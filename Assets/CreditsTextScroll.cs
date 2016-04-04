12603ef74a071a5a5fdfdaed5ba15ea526936dc9
ngbratton <nbratton@purdue.edu>
2016-04-04 17:33:09 -0400

    Credits
    Added Credits scene

---
+++ Assets/CreditsTextScroll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CreditsTextScroll : MonoBehaviour
+{
+	public float speed;
+
+	private void Start()
+	{
+		transform.Translate(-300, (float) -1 * ((Screen.height / 2) + 150), 0);
+	}
+
+	private void Update()
+	{
+		// print (transform.position.y);
+		// print (Screen.height + 150);
+		if (transform.position.y - Screen.height * 1.2 > 3) {
+			// print ("RESET");
+			// print (transform.position.y - Screen.height + 150);
+			transform.Translate(0, (float) -1 * (Screen.height + 500), 0);
+		}
+		transform.Translate(0,speed,0);
+	}
+}
