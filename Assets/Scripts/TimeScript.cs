9141430dad9a9306e59dc94023db7890ba5a3288
Keith Droll <kdroll@purdue.edu>
2016-04-06 13:48:54 -0400

    Added Time to HUD

---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour {
+
+    public Text timeAmount;
+    float truncatedTime, roundedTime; 
+
+
+    // Use this for initialization
+    void Start() {
+        timeAmount = GameObject.Find("Time Amount").GetComponent<Text>();
+        timeAmount.text = "";
+    }
+
+    // Update is called once per frame
+    void Update() {
+        handleTime();
+    }
+
+    void handleTime() {
+        truncatedTime = (float)(System.Math.Truncate((double)Time.time * 100.0) / 100.0);
+        roundedTime = (float)(System.Math.Round((double)truncatedTime, 2));
+        timeAmount.text = "" + roundedTime;
+    }
+}
