fdb95c1b8d8536bbedcde70e860327b8a7c786fd
rdesalvio <ryandesalvio@Ryans-MacBook-Pro-6.local>
2016-04-08 00:42:20 -0400

    added Nunchucks
    they work

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class WeaponPickup : MonoBehaviour {
 
 	static bool pitchfork = false;
+	static bool nunchuck = false;
 	static bool hands = true;
 	static bool pickedUp = false;
 	static bool sword = false;
@@ -17,7 +18,12 @@ public class WeaponPickup : MonoBehaviour {
 	void Update () {
 		
 	}
-
+	public static bool getNunchuck() {
+		return nunchuck;
+	}
+	public static void setNunchuck(bool input) {
+		nunchuck = input;
+	}
 	public static bool getSword() {
 		return sword;
 	}
@@ -47,17 +53,28 @@ public class WeaponPickup : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D collision) {
-		if (collision.gameObject.tag == "Pitchfork" && sword != true) {
+		if (collision.gameObject.tag == "Pitchfork" && sword != true && nunchuck != true) {
 			pickedUp = true;
 			pitchfork = true;
 			hands = false;
+			nunchuck = false;
+			sword = false;
 			GameObject.FindGameObjectWithTag ("Pitchfork").transform.position = new Vector2 (-20f, -20f);
-		} else if (collision.gameObject.tag == "Sword" && pitchfork != true) {
+		} else if (collision.gameObject.tag == "Sword" && pitchfork != true && nunchuck != true) {
 			pickedUp = true;
 			sword = true;
 			pitchfork = false;
+			nunchuck = false;
 			hands = false;
 			GameObject.FindGameObjectWithTag ("Sword").transform.position = new Vector2 (-40f, -40f);
 		}
+		else if (collision.gameObject.tag == "Nunchuck" && pitchfork != true && sword != true) {
+			pickedUp = true;
+			nunchuck = true;
+			pitchfork = false;
+			hands = false;
+			sword = false;
+			GameObject.FindGameObjectWithTag ("Nunchuck").transform.position = new Vector2 (-40f, -40f);
 	}
+}
 }
