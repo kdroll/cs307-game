08484e5fecdfb99c4bca6268aaf53609f0a24a47
rdesalvio <ryandesalvio@pal-nat186-118-175.itap.purdue.edu>
2016-04-13 17:19:44 -0400

    sadfa

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -8,8 +8,6 @@ public class WeaponPickup : MonoBehaviour {
 	static bool pickedUp = false;
 	static bool sword = false;
 	Animator anim;
-	public GameObject grenade;
-	public Transform spawnGrenade;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +15,6 @@ public class WeaponPickup : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.G)) {
-			Instantiate (grenade, spawnGrenade.position, Quaternion.identity);
-		}
 		
 	}
 	public static bool getNunchuck() {
