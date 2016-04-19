3b8e75aa657ca13cb99a656bfbe5f26a0e8201d4
rdesalvio <ryandesalvio@Ryans-MacBook-Pro-6.local>
2016-04-19 15:48:45 -0400

    added all boss attacks

---
+++ Assets/Scripts/TheBossCircleAttack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheBossCircleAttack : MonoBehaviour {
+
+	public GameObject circleParticle;
+	GameObject circleAttack;
+	int times = 0;
+	// Use this for initialization
+	void Start () {
+		circleAttack = (GameObject)Instantiate (circleParticle, gameObject.transform.position, Quaternion.identity);
+		circleAttack.SetActive (false);
+	}
+
+	//IEnumerator wait() {
+	//	yield return new WaitForSeconds (0.3f);
+	//	StartCoroutine (fireballs ());
+	//}
+	IEnumerator fireballs() {
+		circleAttack.SetActive (true);
+		times = 1;
+		yield return new WaitForSeconds (3f);
+		circleAttack.SetActive (false);
+		times = 0;
+		print ("its false");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.FindGameObjectWithTag ("TheBoss").GetComponent<Animator> ().GetBool ("IfCircleAttack") == true && times != 1) {
+			circleAttack.transform.position = gameObject.transform.position;
+			StopCoroutine (fireballs ());
+			StartCoroutine (fireballs ());
+
+			//StopCoroutine (wait ());
+			//StartCoroutine (wait ());
+		}
+
+	
+	}
+}
